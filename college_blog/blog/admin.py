d9643e1d0f01bb3f103f3d3b8571e4c4a95f14c0
Pousali <pousalidolai59@gmail.com>
2026-03-06 15:08:06 +0000

    added

--- college_blog/blog/admin.py
+++ college_blog/blog/admin.py
@@ -3,8 +3,6 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    # This displays these columns in the list view
     list_display = ['title', 'slug']
     
-    # This automatically fills the slug field based on the title
     prepopulated_fields = {'slug': ('title',)}
